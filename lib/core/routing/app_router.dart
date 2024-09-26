1b7670addeec2cd6af83c0131d9c4078ec636c84
emantarekshabana <emantarekshabana@gmail.com>
2024-09-26 15:25:06 +0300

    - added images & svgs folders for assets. - added flutter_svg & added flutter_screenutil - made a file for doc app client - added Navigation extension - added routing folder that contains router class and routes file - added colors & text styles - added empty login screen - added onboarding screen

---
+++ lib/core/routing/app_router.dart
@@ -0,0 +1,34 @@
+import 'package:advanced_project/core/routing/routes.dart';
+import 'package:advanced_project/feature/login/ui/login_screen.dart';
+import 'package:flutter/material.dart';
+
+import '../../feature/onboarding/onboarding_screen.dart';
+
+
+class AppRouter {
+
+
+  Route generateRoute(RouteSettings settings) {
+    // this argument to be passed in any screen like this(argument as className)
+    final arguments = settings.arguments;
+
+    switch (settings.name) {
+      case Routes.onBoardingScreen:
+        return MaterialPageRoute(
+          builder: (_) => const OnboardingScreen(),
+        );
+      case Routes.loginScreen:
+        return MaterialPageRoute(
+          builder: (_) => const LoginScreen(),
+        );
+      default:
+        return MaterialPageRoute(
+          builder: (_) => Scaffold(
+            body: Center(
+              child: Text('No route defined for ${settings.name}'),
+            ),
+          ),
+        );
+    }
+  }
+}
